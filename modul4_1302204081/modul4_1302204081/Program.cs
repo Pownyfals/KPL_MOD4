6b9ed20aaa5b282b22f34d347a65e8b6c0355058
Kevin Febrian <pownyfals@gmail.com>
2022-03-19 17:09:11 +0700

    fix conflict

--- modul4_1302204081/modul4_1302204081/Program.cs
+++ modul4_1302204081/modul4_1302204081/Program.cs
@@ -1,16 +1,14 @@
 ﻿using modul4_1302204081;
 
-<<<<<<< HEAD
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
 //jumlahTigaAngka(13, 22, 40);
 Penjumlahan<double> test = new Penjumlahan<double>();
 
 
-=======
+
 SimpleDataBase<int> data = new SimpleDataBase<int>();
 data.addNewData(13);
 data.addNewData(02);
 data.addNewData(20);
 data.printAllData();
->>>>>>> implementasi-generic-class
+
